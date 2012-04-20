a52f6d6b5bf8dde15227a4f06064615e5000a8ed
David McKain <david.mckain@ed.ac.uk>
2012-04-20 11:24:12 +0100

    Fixed name in license header

--- AsciiMathParser.js
+++ AsciiMathParser.js
@@ -3,20 +3,20 @@
 Copyright (c) 2011-2012, The University of Edinburgh
 All Rights Reserved
 
-This file is part of AsciiMathParser.
+This file is part of AsciiMathParser.js
 
-AsciiMathParser is free software; you can redistribute it and/or modify it
+AsciiMathParser.js is free software; you can redistribute it and/or modify it
 under the terms of the GNU Lesser General Public License as published by the
 Free Software Foundation; either version 3 of the License, or (at your
 option) any later version.
 
-AsciiMathParser is distributed in the hope that it will be useful, but WITHOUT
+AsciiMathParser.js is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License (at
 http://www.gnu.org/licences/lgpl.html) for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with AsciiMathParser. If not, see <http://www.gnu.org/licenses/lgpl.html>.
+along with AsciiMathParser.js. If not, see <http://www.gnu.org/licenses/lgpl.html>.
 
 AsciiMathParser.js
 ==================
